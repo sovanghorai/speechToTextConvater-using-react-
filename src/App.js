18e655968dc5bddcef17cc8d5a60537386abbb66
sovanghorai <sovanghorai6@gmail.com>
2024-02-12 20:06:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from 'react-use-clipboard'
+
+
+const App = () => {
+  const [textToCopy,setTextToCopy] = useState();
+  const [isCopyed,setCopied] = useClipboard (textToCopy);
+  const startListening = ()=> SpeechRecognition.startListening({ continuous: true, language:'en-IN'});
+
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
+        <p>A React hook that converts speech from the microphone to text and makes it available to your React
+          components.</p>
+        <div className="main-content" onClick={()=>setTextToCopy(transcript)}>
+            {transcript}
+        </div>
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopyed ? 'Copyed' : 'Copy to clickboard'}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+        </div>
+
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+
